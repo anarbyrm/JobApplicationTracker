4e9a14a1c136513825cec3429449c5db54d58f89
Anar Bayramov <anar.bayram@yahoo.com>
2024-05-19 22:23:41 +0400

    feat: Implement ApplicationRepository

--- JobApplicationTracker.DataAccess/Repositories/Implementations/ApplicationRepository.cs
+++ JobApplicationTracker.DataAccess/Repositories/Implementations/ApplicationRepository.cs
@@ -1,5 +1,6 @@
 ﻿using JobApplicationTracker.Core.Entities;
 using JobApplicationTracker.DataAccess.DbContexts;
+using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 
 namespace JobApplicationTracker.DataAccess.Repositories
@@ -15,27 +16,39 @@ namespace JobApplicationTracker.DataAccess.Repositories
 
         public async Task<List<JobApplication>> GetAllAsync(Expression<Func<JobApplication, bool>> predicate)
         {
-            throw new NotImplementedException();
+            IQueryable<JobApplication> query = _dbContext.Applications;
+            if (predicate != null)
+                query = query.Where(predicate);
+            return await query.ToListAsync();
         }
 
         public async Task<JobApplication> GetFirstAsync(Expression<Func<JobApplication, bool>> predicate)
         {
-            throw new NotImplementedException();
+            return await _dbContext.Applications.FirstOrDefaultAsync(predicate);
         }
 
         public async Task<bool> AddAsync(JobApplication entity)
         {
-            throw new NotImplementedException();
+            _dbContext.Applications.Add(entity);
+            return await SaveAsync();
         }
 
         public async Task<bool> Update(JobApplication entity)
         {
-            throw new NotImplementedException();
+            _dbContext.Applications.Update(entity);
+            return await SaveAsync();
         }
 
         public async Task<bool> DeleteAsync(JobApplication entity)
         {
-            throw new NotImplementedException();
+            _dbContext.Applications.Remove(entity);
+            return await SaveAsync();
+        }
+
+        private async Task<bool> SaveAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+            return true;
         }
     }
 }
