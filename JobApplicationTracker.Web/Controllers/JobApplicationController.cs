2d72c5bf920f668ce9429e7acf254d41bec094b8
Anar Bayramov <anar.bayram@yahoo.com>
2024-05-21 15:42:50 +0400

    feat: Add job application controller

---
+++ JobApplicationTracker.Web/Controllers/JobApplicationController.cs
@@ -0,0 +1,32 @@
+﻿using JobApplicationTracker.Application.Models;
+using JobApplicationTracker.Application.Services;
+using JobApplicationTracker.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobApplicationTracker.Web.Controllers
+{
+    [Route("applications")]
+    public class JobApplicationController : Controller
+    {
+        private IJobApplicationService _service;
+
+        public JobApplicationController(IJobApplicationService service)
+        {
+            _service = service;
+        }
+
+        [Route("")]
+        public async Task<IActionResult> List()
+        {
+            //List<JobApplicationListViewModel> applications = await _service.GetAllAsync();
+            return View();
+        }
+
+        [Route("{id}")]
+        public async Task<IActionResult> Detail(Guid id)
+        {
+            var application = await _service.GetOneByIdAsync(id);
+            return View(application);
+        }
+    }
+}
